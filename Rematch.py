b076755296920177e83b5e4f96cf7225fe239b4c
Soukamila <152564494+Soukamila@users.noreply.github.com>
2023-12-09 18:10:00 -0800

    Update Rematch.py

--- Rematch.py
+++ Rematch.py
@@ -1,6 +1,19 @@
-mo = 'hiddenendpass'
-pozisyon_start = mo.find('hidden') + len('hidden')
-pozisyon_end = mo.find('endpass')
+def jwenn_karakte_nan_mitan(chenn):
+    mots = chenn.split()   
 
-karakte_nan_mitan = mo[pozisyon_start:pozisyon_end]
-print(karakte_nan_mitan)
+    for mo in mots:
+        if 'hidden' in mo or 'endpass' in mo:
+            milieu = len(mo) // 2  
+            karakte_nan_mitan = mo[milieu]  
+            return karakte_nan_mitan 
+
+    return None 
+
+'
+chenn_karakte = input("Entrer yon chaine ki gen hidden ak enpass la danl")
+resultat = jwenn_karakte_nan_mitan(chenn_karakte)
+
+if resultat:
+    print(f"Karaktè ki nan mitan: {resultat}")
+else:
+    print("Pa gen karaktè ki nan mitan 'hidden' ak 'endpass' nan chenn sa a.")
